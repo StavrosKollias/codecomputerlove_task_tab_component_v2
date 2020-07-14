70a5017faeb247c76383aa56536093977aeca104
Stavros Kollias <kollias1252@gmail.com>
2020-07-14 23:34:20 +0100

    created Add new Tab click handler

--- tabs.js
+++ tabs.js
@@ -1,8 +1,3 @@
-function getDomElements(type) {
-   var elements = document.querySelectorAll(type);
-   return elements;
-}
-
 function idNumber(id, i) {
    return id.split("-")[i];
 }
@@ -16,6 +11,89 @@ function handleClicktab(element) {
       var blockIdNumber = idNumber(tabBlocks[i].id, 2);
       blockIdNumber == tabSelected ? tabBlocks[i].classList.add("active-tab-block") : tabBlocks[i].classList.remove("active-tab-block");
    });
-
    element.classList.add("active-tab");
+}
+
+function addNewTab() {
+   const tabItems = getDomElements(".tab-item");
+   const newItemEnum = tabItems.length + 1;
+   var tabContainer = document.getElementById("tabs-container");
+   const tabBlocksContainer = document.getElementById("tab-blocks-container");
+   var blockDiv = createTabblock();
+   console.log(blockDiv);
+   tabBlocksContainer.appendChild(blockDiv);
+   addAttributeElement(blockDiv, "class", "tab-blocks-item");
+   addAttributeElement(blockDiv, "id", `tab-block-${newItemEnum}`);
+   const tab = createTabButton();
+   tabContainer.appendChild(tab);
+   addAttributeElement(tab, "class", `tab-item`);
+   addAttributeElement(tab, "id", `tab-${newItemEnum}`);
+   addEventListenerNewTab(tab);
+   handleClicktab(tab);
+}
+
+function createTabblock() {
+   console.log("create Tab Block");
+   const blockDiv = document.createElement("div");
+   const blocktitle = createTitle();
+   const blockTextContainer = createTextContainer();
+   const blockTxtPar = createTextPar();
+   const blockTxtPar2 = createTextPar();
+   blockDiv.appendChild(blocktitle);
+   addAttributeElement(blocktitle, "class", "tab-blocks-item_title");
+   blockTextContainer.appendChild(blockTxtPar);
+   addAttributeElement(blockTxtPar, "class", "tab-blocks-text_item");
+   blockTextContainer.appendChild(blockTxtPar2);
+   addAttributeElement(blockTxtPar2, "class", "tab-blocks-text_item");
+   blockDiv.appendChild(blockTextContainer);
+   blockTextContainer;
+   addAttributeElement(blockTextContainer, "class", "tab-blocs_text");
+   blockDiv.appendChild(blockTextContainer);
+   return blockDiv;
+}
+
+function createTitle() {
+   console.log("create Title block");
+   const blocktitle = document.createElement("h2");
+   const titleTxt = document.createTextNode("New Title here");
+   blocktitle.appendChild(titleTxt);
+   return blocktitle;
+}
+
+function createTextContainer() {
+   console.log("create Title block");
+   const blockTextContainer = document.createElement("div");
+   return blockTextContainer;
+}
+
+function createTextPar() {
+   console.log("create Title block");
+   const blockTxtPar = document.createElement("p");
+   const parTxt = document.createTextNode(
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque omnis perferendis tempore nulla molestiae totam veritatis possimus aliquid provident rem neque optio dolorum, fugiat esse minima, asperiores quas quia facilis."
+   );
+   blockTxtPar.appendChild(parTxt);
+   return blockTxtPar;
+}
+
+function createTabButton() {
+   const tab = document.createElement("button");
+   const btnTxt = document.createTextNode("New Tab Name");
+   tab.appendChild(btnTxt);
+   return tab;
+}
+function addEventListenerNewTab(tab) {
+   tab.addEventListener("click", (e) => {
+      handleClicktab(e.target);
+   });
+}
+
+//---- Functions-- /
+function getDomElements(type) {
+   var elements = document.querySelectorAll(type);
+   return elements;
+}
+
+function addAttributeElement(element, type, string) {
+   element.setAttribute(type, string);
 }
